fd8a40b67bcf2ad194888240c0f5fb2fdc12d0db
argostellar <arskud@gmail.com>
2020-10-07 18:02:19 +0700

    Выполняет задание 1 раздела 2 (личный проект)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+
 import MainPage from "../main-page/main-page";
+import SignInPage from "../sign-in-page/sign-in-page";
+import MyListPage from "../my-list-page/my-list-page";
+import FilmPage from "../film-page/film-page";
+import AddReviewPage from "../add-review-page/add-review-page";
+import PlayerPage from "../player-page/player-page";
 
 
 const App = ({filmInfo}) => {
   return (
-    <MainPage filmInfo={filmInfo} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage filmInfo={filmInfo} />
+        </Route>
+        <Route exact path="/login">
+          <SignInPage />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListPage />
+        </Route>
+        <Route exact path="/films/:id">
+          <FilmPage />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewPage />
+        </Route>
+        <Route exact path="/player/:id">
+          <PlayerPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
